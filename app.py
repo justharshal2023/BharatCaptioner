6673e504b94afe78c916f159988edc12cbdb80cd
Harshal <114017724+justharshal2023@users.noreply.github.com>
2024-08-02 00:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,8 @@ from transformers import pipeline
 # Initialize EasyGoogleTranslate
 translator = EasyGoogleTranslate(source_language="en", target_language="hi", timeout=10)
 
-# # Load the Llava model and processor
-# pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-@st.cache
-def load_model():
-    return pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-
-pipe = load_model()
+# # Load the BLIP model and processor
+pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
 
 # Title of the Streamlit app
 st.title("BharatCaptioner")
