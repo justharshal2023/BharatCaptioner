7f2f0e690ce09fc0adaa39c330410a01cd545717
Harshal <114017724+justharshal2023@users.noreply.github.com>
2024-08-01 12:55:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ if url:
         st.image(image, caption="Image from URL.", use_column_width=True)
     except requests.exceptions.RequestException as e:
         st.error(f"Error loading image from URL: {e}")
+        print("Sometimes some url doesn't work we would suggest you to upload an image after downloading that image ;)")
 
 # If an image is uploaded or URL is provided
 if image is not None:
