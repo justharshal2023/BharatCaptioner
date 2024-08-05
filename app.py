f6f2aa6c1c187348ce8953e6934050936eb7697a
Harshal <114017724+justharshal2023@users.noreply.github.com>
2024-08-05 14:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ st.write('A simple tool to identify/describe Indian Landmarks in Indic Languages
 st.sidebar.title("About the Developer")
 st.sidebar.info("""
     **Developer**: Harshal  
-    **For Code**: [My Github](https://github.com/justharshal2023) 
-    **For the model**: [Mail me here](harshal19052003@gmail.com)
+    **Code**: [My Github](https://github.com/justharshal2023) 
+    **Model**: [Mail me here](harshal19052003@gmail.com)
     **Connect with me**: [My LinkedIn](https://www.linkedin.com/in/harshal-123a90250/)
 """)
 
